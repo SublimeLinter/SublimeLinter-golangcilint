b10119d7f412b62353b0865bcdf9aaba05e3d16e
Cixtor <cixtords@gmail.com>
2018-12-04 09:00:00 -0800

    Override SublimeLinter.run method to process in the background

--- linter.py
+++ linter.py
@@ -11,3 +11,13 @@ class Golangcilint(Linter):
     defaults = {"selector": "source.go"}
     error_stream = util.STREAM_STDOUT
     multiline = False
+
+    def run(self, cmd, code):
+        dir = os.path.dirname(self.filename)
+        if not dir:
+            print("golangcilint: skipped linting of unsaved file")
+            return
+        if settings.get("lint_mode") == "background":
+            return self._live_lint(cmd, code)
+        else:
+            return self._in_place_lint(cmd)
