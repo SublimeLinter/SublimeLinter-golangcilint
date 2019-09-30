942a6d5dd52b283f9169b15c1f2f6c0ad3adcf72
Jan Schlicht <jan@d2iq.com>
2019-09-30 09:17:26 +0200

    Use 'tab' output format as regex input.
    This also sets the 'code' capture group.

--- linter.py
+++ linter.py
@@ -2,13 +2,13 @@ from SublimeLinter.lint import Linter, WARNING
 
 
 class GolangCILint(Linter):
-    cmd = 'golangci-lint run --fast'
+    cmd = 'golangci-lint run --fast --out-format tab'
     # Column reporting is optional and not provided by all linters.
     # Issues reported by the 'typecheck' linter are treated as errors,
     # because they indicate code that won't compile. All other linter issues
     # are treated as warnings.
-    regex = r'^(?P<filename>(\w+:\\\\)?.[^:]+):(?P<line>\d+):((?P<col>\d+):)? ' + \
-        r'(?P<message>.+\(((?P<error>typecheck)|\w+)\))$'
+    regex = r'^(?P<filename>(\w+:\\\\)?.[^:]+):(?P<line>\d+)(:(?P<col>\d+))?\s+' + \
+        r'(?P<code>(?P<error>typecheck)|\w+)\s+(?P<message>.+)$'
     default_type = WARNING
     defaults = {
         'selector': 'source.go'
