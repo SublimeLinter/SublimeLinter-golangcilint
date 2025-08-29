0ff1df4e33e125f9bdc19c8fa29dcf472b42aa78
herr kaste <herr.kaste@gmail.com>
2025-08-29 12:40:47 +0200

    Switch to standard text format and adjust regex
    Fixes SublimeLinter/SublimeLinter#1986
    
    - The column is optional but we need to expect possible trailing ":".
      From ale, we copy that there might be multiple colons.
    - Capture the linter name and treat it like "code".  In case the linter
      is "typecheck" treat it as "error".
    
    In general, this regex might be slower but also resilient *if* no linter
    is actually printed.

--- linter.py
+++ linter.py
@@ -2,18 +2,28 @@ from SublimeLinter.lint import Linter, WARNING
 
 
 class GolangCILint(Linter):
-    def cmd(self):
-        if self.settings.get("v1", False):
-            return 'golangci-lint run ${args} --out-format tab ${file_path}'
-        return 'golangci-lint run ${args} --output.tab.path stdout ${file_path}'
     tempfile_suffix = '-'
-    # Column reporting is optional and not provided by all linters.
-    # Issues reported by the 'typecheck' linter are treated as errors,
-    # because they indicate code that won't compile. All other linter issues
-    # are treated as warnings.
-    regex = r'^(?P<filename>(\w+:\\\\)?.[^:]+):(?P<line>\d+)(:(?P<col>\d+))?\s+' + \
-        r'(?P<code>(?P<error>typecheck)|\w+)\s+(?P<message>.+)$'
+    regex = (
+        r'^(?P<filename>(?:\w:)?[^:]+):'
+        # Column reporting is optional and not provided by all linters.
+        r'(?P<line>\d+)(?::(?P<col>\d+))?:*\s+'
+        r'(?P<message>.+?)'
+        # Issues reported by the 'typecheck' linter are treated as errors,
+        # because they indicate code that won't compile.
+        r'(?:\s+\((?:(?P<error>typecheck)|(?P<code>[^()]+))\))?$'
+    )
+    # All other linter issues are treated as warnings.
     default_type = WARNING
     defaults = {
         'selector': 'source.go',
     }
+
+    def cmd(self):
+        if self.settings.get("v1", False):
+            return 'golangci-lint run ${args} --out-format text ${file_path}'
+        return (
+            'golangci-lint run ${args}'
+            ' --output.text.path=stdout'
+            ' --show-stats=0'
+            ' ${file_path}'
+        )
